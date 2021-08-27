1a9b768b37c7cc4b0447f730e985829300e0614b
Manuele Pesenti <manuele.pesenti@gter.it>
2021-08-27 17:30:42 +0200

    HighCharts integration and first example with temperature

--- src/pages/LagoLugano.vue
+++ src/pages/LagoLugano.vue
@@ -12,14 +12,14 @@
                       <td valign="center" width="65%">
                         <div class="col-xl-4" style="max-width:100%;">
                           <card>
-                            Lorem ipsum dolor sit amet. Ea dignissimos aspernatur et quaerat quia et quasi ipsa et voluptates debitis sit ipsum laudantium sed nobis accusantium quo quidem debitis. 
-                            Non possimus dolorum qui dolores optio aut quasi dolorem. In adipisci voluptatem et consectetur sequi aut recusandae iste. Non nesciunt earum qui tempora maiores At dolorem sunt quo quia officiis. 
+                            Lorem ipsum dolor sit amet. Ea dignissimos aspernatur et quaerat quia et quasi ipsa et voluptates debitis sit ipsum laudantium sed nobis accusantium quo quidem debitis.
+                            Non possimus dolorum qui dolores optio aut quasi dolorem. In adipisci voluptatem et consectetur sequi aut recusandae iste. Non nesciunt earum qui tempora maiores At dolorem sunt quo quia officiis.
                             Sed facilis sunt in omnis dolor et consequatur autem aut ducimus nisi ea eius quibusdam et omnis quasi? Qui nobis mollitia eos perspiciatis asperiores et magni sequi? Non quos consequatur 33 sint facilis qui deserunt quae ex nemo illum et distinctio voluptas.<br><br>
-                            Ad libero obcaecati a atque quibusdam sed commodi iusto 33 maiores et minus debitis qui dolores dolor eum corrupti laudantium. Et exercitationem pariatur ut ipsa eveniet quo necessitatibus odit aut rerum harum et maiores odio et dolores amet ut inventore incidunt. 
-                            Ut voluptates molestias et voluptatem beatae id fugit velit et voluptas expedita? Incidunt sint qui officiis iusto At quidem quis. In maxime omnis et nulla eaque qui molestiae fugiat 33 reiciendis inventore qui reiciendis totam in fuga assumenda. Sed amet distinctio 
+                            Ad libero obcaecati a atque quibusdam sed commodi iusto 33 maiores et minus debitis qui dolores dolor eum corrupti laudantium. Et exercitationem pariatur ut ipsa eveniet quo necessitatibus odit aut rerum harum et maiores odio et dolores amet ut inventore incidunt.
+                            Ut voluptates molestias et voluptatem beatae id fugit velit et voluptas expedita? Incidunt sint qui officiis iusto At quidem quis. In maxime omnis et nulla eaque qui molestiae fugiat 33 reiciendis inventore qui reiciendis totam in fuga assumenda. Sed amet distinctio
                             vel possimus amet At voluptas dolorem aut eaque internos. Ut ducimus tenetur et nihil quaerat ea nulla laudantium aut numquam possimus in cumque aliquam. Et perspiciatis earum cum tenetur ipsum est asperiores voluptate eum doloremque molestiae. Et saepe eius sed sapiente natus At laudantium magni.<br><br>
-                            Est consequuntur similique sed Quis corrupti sed porro obcaecati et quia magnam. Ut blanditiis explicabo vel omnis exercitationem qui facere blanditiis At dolorem quia. Non ducimus repellendus cum unde tenetur et similique quod qui pariatur possimus ea ipsum maxime. 
-                            Et veniam laudantium eos tempore voluptatem qui deleniti nihil et consectetur dolore? Sit illum explicabo qui architecto nobis qui mollitia consequuntur qui odit galisum ut quae tenetur et ipsam magni est internos ducimus. Qui voluptatibus ipsa ut itaque dolore et 
+                            Est consequuntur similique sed Quis corrupti sed porro obcaecati et quia magnam. Ut blanditiis explicabo vel omnis exercitationem qui facere blanditiis At dolorem quia. Non ducimus repellendus cum unde tenetur et similique quod qui pariatur possimus ea ipsum maxime.
+                            Et veniam laudantium eos tempore voluptatem qui deleniti nihil et consectetur dolore? Sit illum explicabo qui architecto nobis qui mollitia consequuntur qui odit galisum ut quae tenetur et ipsam magni est internos ducimus. Qui voluptatibus ipsa ut itaque dolore et
                             corrupti magni hic sunt fugiat et facere harum ut libero accusantium ut molestiae tempora! Sed animi sunt non quasi explicabo sit voluptates porro qui harum omnis aut perspiciatis corrupti qui architecto consectetur in delectus iste.
                           </card>
                         </div>
@@ -174,163 +174,26 @@
         </div>
       </div-->
 
+
+
       <div class="row">
         <div class="col-12">
           <card>
             <div class="typo-line">
-              <p class="longtext"><span class="category"><b><i>Chlorophyll-a</i></b></span><br></p>
-              
-          <div class="row">
-          <div class="col-md-8">
-          <chart-card :chart-data="lineChart.data"
-                      :chart-options="lineChart.options"
-                      :responsive-options="lineChart.responsiveOptions">
-            <template slot="header">
-              <h4 class="card-title">Users Behavior</h4>
-              <p class="card-category">24 Hours performance</p>
-            </template>
-            <template slot="footer">
-              <div class="legend">
-                <i class="fa fa-circle text-info"></i> Open
-                <i class="fa fa-circle text-danger"></i> Click
-                <i class="fa fa-circle text-warning"></i> Click Second Time
-              </div>
-              <hr>
-              <div class="stats">
-                <i class="fa fa-history"></i> Updated 3 minutes ago
-              </div>
-            </template>
-          </chart-card>
-        </div>
+              <p class="longtext"><span class="category"><b><i>Temperature</i></b></span><br></p>
 
-        <div class="col-md-4">
-          <chart-card :chart-data="pieChart.data" chart-type="Pie">
-            <template slot="header">
-              <h4 class="card-title">Email Statistics</h4>
-              <p class="card-category">Last Campaign Performance</p>
-            </template>
-            <template slot="footer">
-              <div class="legend">
-                <i class="fa fa-circle text-info"></i> Open
-                <i class="fa fa-circle text-danger"></i> Bounce
-                <i class="fa fa-circle text-warning"></i> Unsubscribe
+              <div class="row">
+                <div class="col-md-12">
+                  <highcharts :options="tempChartOptions" style="height: 115px;"></highcharts>
+                </div>
               </div>
-              <hr>
-              <div class="stats">
-                <i class="fa fa-clock-o"></i> Campaign sent 2 days ago
-              </div>
-            </template>
-          </chart-card>
-        </div>
-        </div>
-        </div>
-          </card>
-        </div>
-      </div>
 
-      <div class="row">
-        <div class="col-md-8">
-          <chart-card :chart-data="lineChart.data"
-                      :chart-options="lineChart.options"
-                      :responsive-options="lineChart.responsiveOptions">
-            <template slot="header">
-              <h4 class="card-title">Users Behavior</h4>
-              <p class="card-category">24 Hours performance</p>
-            </template>
-            <template slot="footer">
-              <div class="legend">
-                <i class="fa fa-circle text-info"></i> Open
-                <i class="fa fa-circle text-danger"></i> Click
-                <i class="fa fa-circle text-warning"></i> Click Second Time
-              </div>
-              <hr>
-              <div class="stats">
-                <i class="fa fa-history"></i> Updated 3 minutes ago
-              </div>
-            </template>
-          </chart-card>
-        </div>
 
-        <div class="col-md-4">
-          <chart-card :chart-data="pieChart.data" chart-type="Pie">
-            <template slot="header">
-              <h4 class="card-title">Email Statistics</h4>
-              <p class="card-category">Last Campaign Performance</p>
-            </template>
-            <template slot="footer">
-              <div class="legend">
-                <i class="fa fa-circle text-info"></i> Open
-                <i class="fa fa-circle text-danger"></i> Bounce
-                <i class="fa fa-circle text-warning"></i> Unsubscribe
-              </div>
-              <hr>
-              <div class="stats">
-                <i class="fa fa-clock-o"></i> Campaign sent 2 days ago
-              </div>
-            </template>
-          </chart-card>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-6">
-          <chart-card
-            :chart-data="barChart.data"
-            :chart-options="barChart.options"
-            :chart-responsive-options="barChart.responsiveOptions"
-            chart-type="Bar">
-            <template slot="header">
-              <h4 class="card-title">2014 Sales</h4>
-              <p class="card-category">All products including Taxes</p>
-            </template>
-            <template slot="footer">
-              <div class="legend">
-                <i class="fa fa-circle text-info"></i> Tesla Model S
-                <i class="fa fa-circle text-danger"></i> BMW 5 Series
-              </div>
-              <hr>
-              <div class="stats">
-                <i class="fa fa-check"></i> Data information certified
-              </div>
-            </template>
-          </chart-card>
-        </div>
-
-        <!--div class="col-md-6">
-          <card>
-            <template slot="header">
-              <h5 class="title">Tasks</h5>
-              <p class="category">Backend development</p>
-            </template>
-            <l-table :data="tableData.data"
-                     :columns="tableData.columns">
-              <template slot="columns"></template>
-
-              <template slot-scope="{row}">
-                <td>
-                  <base-checkbox v-model="row.checked"></base-checkbox>
-                </td>
-                <td>{{row.title}}</td>
-                <td class="td-actions text-right">
-                  <button type="button" class="btn-simple btn btn-xs btn-info" v-tooltip.top-center="editTooltip">
-                    <i class="fa fa-edit"></i>
-                  </button>
-                  <button type="button" class="btn-simple btn btn-xs btn-danger" v-tooltip.top-center="deleteTooltip">
-                    <i class="fa fa-times"></i>
-                  </button>
-                </td>
-              </template>
-            </l-table>
-            <div class="footer">
-              <hr>
-              <div class="stats">
-                <i class="fa fa-history"></i> Updated 3 minutes ago
-              </div>
             </div>
           </card>
-
-        </div-->
+        </div>
       </div>
+
     </div>
   </div>
 </template>
@@ -340,7 +203,46 @@
   import LTable from 'src/components/Table.vue'
   import { latLngBounds, latLng } from "leaflet";
   import { LMap, LTileLayer, LWMSTileLayer, LControlLayers } from "vue2-leaflet";
-  import axios from 'axios'
+  import axios from 'axios';
+
+  import {Chart} from 'highcharts-vue';
+
+  import Highcharts from 'highcharts';
+  import loadBullet from 'highcharts/modules/bullet.js';
+  loadBullet(Highcharts);
+
+  Highcharts.setOptions({
+      chart: {
+          inverted: true,
+          marginLeft: 135,
+          type: 'bullet'
+      },
+      title: {
+          text: null
+      },
+      legend: {
+          enabled: false
+      },
+      yAxis: {
+          gridLineWidth: 0
+      },
+      plotOptions: {
+          series: {
+              pointPadding: 0.25,
+              borderWidth: 0,
+              color: '#000',
+              targetOptions: {
+                  width: '200%'
+              }
+          }
+      },
+      credits: {
+          enabled: false
+      },
+      exporting: {
+          enabled: false
+      }
+  });
 
   export default {
     components: {
@@ -350,92 +252,53 @@
       LMap,
       LTileLayer,
       "l-wms-tile-layer": LWMSTileLayer,
-      LControlLayers
+      LControlLayers,
+      highcharts: Chart
     },
     data () {
       return {
+        tempChartOptions: {
+            chart: {
+                marginTop: 40
+            },
+            title: {
+                text: '2017 YTD'
+            },
+            xAxis: {
+                categories: ['<span class="hc-cat-title">Revenue</span><br/>U.S. $ (1,000s)']
+            },
+            yAxis: {
+                plotBands: [{
+                    from: -10,
+                    to: 4,
+                    color: 'cyan'
+                }, {
+                    from: 4,
+                    to: 8,
+                    color: 'blue'
+                }, {
+                    from: 8,
+                    to: 16,
+                    color: 'orange'
+                }, {
+                    from: 16,
+                    to: 30,
+                    color: 'red'
+                }],
+                title: null
+            },
+            series: [{
+                data: [{
+                    y: 5.5,
+                    target: 10
+                }]
+            }],
+            tooltip: {
+                pointFormat: '<b>{point.y}</b> (with target at {point.target})'
+            }
+        },
         editTooltip: 'Edit Task',
         deleteTooltip: 'Remove',
-        pieChart: {
-          data: {
-            labels: ['40%', '20%', '40%'],
-            series: [40, 20, 40]
-          }
-        },
-        lineChart: {
-          data: {
-            labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
-            series: [
-              [287, 385, 490, 492, 554, 586, 698, 695],
-              [67, 152, 143, 240, 287, 335, 435, 437],
-              [23, 113, 67, 108, 190, 239, 307, 308]
-            ]
-          },
-          options: {
-            low: 0,
-            high: 800,
-            showArea: false,
-            height: '245px',
-            axisX: {
-              showGrid: false
-            },
-            lineSmooth: true,
-            showLine: true,
-            showPoint: true,
-            fullWidth: true,
-            chartPadding: {
-              right: 50
-            }
-          },
-          responsiveOptions: [
-            ['screen and (max-width: 640px)', {
-              axisX: {
-                labelInterpolationFnc (value) {
-                  return value[0]
-                }
-              }
-            }]
-          ]
-        },
-        barChart: {
-          data: {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            series: [
-              [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-              [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
-            ]
-          },
-          options: {
-            seriesBarDistance: 10,
-            axisX: {
-              showGrid: false
-            },
-            height: '245px'
-          },
-          responsiveOptions: [
-            ['screen and (max-width: 640px)', {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc (value) {
-                  return value[0]
-                }
-              }
-            }]
-          ]
-        },
-        tableData: {
-          data: [
-            {title: 'Sign contract for "What are conference organizers afraid of?"', checked: false},
-            {title: 'Lines From Great Russian Literature? Or E-mails From My Boss?', checked: true},
-            {
-              title: 'Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit',
-              checked: true
-            },
-            {title: 'Create 4 Invisible User Experiences you Never Knew About', checked: false},
-            {title: 'Read "Following makes Medium better"', checked: false},
-            {title: 'Unfollow 5 enemies from twitter', checked: false}
-          ]
-        },
         zoom: 13,
         //center: latLng(47.41322, -1.219482),
         bounds: latLngBounds([
@@ -597,6 +460,9 @@
       }).catch(error => {
         console.error(error);
       });
+
+      this.fetchTempData();
+
     },
       methods: {
         zoomUpdate(zoom) {
@@ -604,6 +470,35 @@
         },
         centerUpdate(center) {
           this.currentCenter = center;
+        },
+        fetchTempData() {
+          var self = this;
+          const path_params = {
+            services: 'demo',
+            offerings: 'temporary',
+            procedures: 'GHIFFA_EPILIMNIOM',
+            observedproperties: 'rn:ogc:def:parameter:x-istsos:1.0:water:temperature'
+          }
+
+          var config = {
+            method: 'get',
+            url: `https://istsos.ddns.net/istsos/wa/istsos/services/${path_params.services}/operations/getobservation/offerings/${path_params.offerings}/procedures/${path_params.procedures}/observedproperties/${path_params.observedproperties}/eventtime/last`,
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + window.localStorage.getItem('kcAccessToken')
+            }
+          };
+
+          axios(config).then(function (response) {
+            const dataArray = response.data.data[0].result.DataArray;
+            self.tempChartOptions.title.text = dataArray.field[1].name;
+            self.tempChartOptions.xAxis.categories[0] = `<span class="hc-cat-title">uom</span><br/>${dataArray.field[1].uom}`;
+            const value = dataArray.values[0][1];
+            self.tempChartOptions.series[0].data[0].y = value;
+          }).catch(function (error) {
+            console.log(error);
+          });
+
         }
       }
   }
