fdbb7bc7f461f0a217ed9c48a25709cf36147f64
Gioia Figlioli <82337897+joynoel83@users.noreply.github.com>
2022-10-27 11:21:07 +0200

    dislaimer footer

--- src/components/disclaimerMessage.vue
+++ src/components/disclaimerMessage.vue
@@ -13,6 +13,12 @@
             {{ buttonTextDeny }}
     </div>
 </div> -->
+
+<div>
+  <a @click="showComponent()"> 
+        Disclaimer
+  </a>
+  
 <div class="row" v-if="isOpen">
       <div class="col-md-4 col-sm-12 button-fixed">
         <div class="p-3 pb-4 bg-custom text-white">
@@ -48,7 +54,7 @@
         </div>
       </div>
     </div>
-
+</div> 
 </template>
 
 <script>
@@ -76,7 +82,11 @@ export default{
         deny(){
             this.isOpen = false;
             localStorage.setItem('Consent:accepted', false)
-        }
+        },
+        showComponent(){
+        this.isOpen=true;
+        console.log(this.isOpen)
+      }
     }
 }
 </script>
@@ -96,5 +106,8 @@ export default{
       }
       p{
       font-size: 14px;
+      }
+      a{
+        cursor: pointer;
       }
     </style>
